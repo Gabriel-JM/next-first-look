5f5ac7d93f0eb54062687f4fdf206973f865701a
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-01-22 12:00:14 -0300

    Applying getInitialProps to home page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,31 @@
-function Home() {
-  return <h1>Home</h1>
+interface HomeProps {
+  ownersList: Array<{
+    name: string
+    vehicle: string
+  }>
+}
+
+function Home({ ownersList }: HomeProps) {
+  return (
+    <div>
+      {ownersList.map((owner, index) => (
+        <div key={index}>
+          {owner.name}'s {owner.vehicle}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+Home.getInitialProps = () => {
+  return {
+    ownersList: [
+      {
+        name: 'Gabriel',
+        vehicle: 'Bike'
+      }
+    ]
+  }
 }
 
 export default Home
