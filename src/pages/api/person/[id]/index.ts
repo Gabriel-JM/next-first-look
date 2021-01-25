0c8ada1b67bde983026a47b9cd92a7dce520555d
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-01-25 11:54:16 -0300

    Defining API endpoints

---
+++ src/pages/api/person/[id]/index.ts
@@ -0,0 +1,5 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default function getPersonById(req: NextApiRequest, res: NextApiResponse) {
+
+}
---
+++ src/pages/api/person/[id]/vehicles.ts
@@ -0,0 +1,5 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default function getAllVehicleByPersonId(req: NextApiRequest, res: NextApiResponse) {
+
+}
